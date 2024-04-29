62ec847ed02adc068ba708145b812af4233c5f46
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-04-29 23:59:54 +0700

    The first code
    Only about MATH QUIZZES

---
+++ TelwaqR.py
@@ -0,0 +1,94 @@
+print("MATH QUIZ!!!")
+print()
+UserName = input("What is your username? ")
+print()
+print("Hello Welcome,",UserName)
+print()
+print("Press 'y' to continue")
+Prep = input("Do you ready to answer this quiz? ")
+if Prep == "y":
+    print()
+    print("NICE", UserName)
+    print()
+else:
+    quit()
+quiz = input("2 + 2 = ")
+if quiz == "4":
+    print()
+    print("Good Job!", UserName)
+    print()
+else:
+    print()
+    print("Pls try again!", UserName)
+    print()
+    exit()
+quiz = input("5 + 2 = ")
+if quiz == "7":
+    print()
+    print("Great!", UserName)
+    print()
+else:
+    print()
+    print("Try again!", UserName)
+    print()
+    exit()
+quiz = input("9 + 8 = ")
+if quiz == "17":
+    print()
+    print("Nice!", UserName)
+    print()
+else:
+    print()
+    print("Don't give up!", UserName)
+    print()
+    exit()
+quiz = input("45 + 34 = ")
+if quiz == "79":
+    print()
+    print("Great Job!", UserName)
+    print()
+else:
+    print()
+    print("Wrong!", UserName)
+    print()
+    exit()
+quiz = input("198 + 342 = ")
+if quiz == "540":
+    print()
+    print("Correct!", UserName)
+    print()
+else:
+    print()
+    print("Adios", UserName)
+    print()
+    exit()
+quiz = input("8934 + 9586 = ")
+if quiz == "18520":
+    print()
+    print("Wow!", UserName)
+    print()
+else:
+    print()
+    print("auf Wiedersehen!", UserName)
+    print()
+    exit()
+quiz = input("1450587 + 5689209 = ")
+if quiz == "7139796":
+    print()
+    print("Well Done!", UserName)
+    print()
+else:
+    print()
+    print("Close!", UserName)
+    print()
+    exit()
+print("Hooray! you finished the quiz, WELL DONE!", UserName)
+print()
+print("Please give me some FEEDBACK for me to improve my programming skill", UserName)
+print()
+print("Vielen Dank")
+print()
+quit()
+
+# *NOTES*
+# *I'm still Newbie at programming
